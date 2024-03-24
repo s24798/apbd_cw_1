067271352c4bd79d0b5f85c4ecc64298a898a185
s24798 <s24798@pjwstk.edu.pl>
2024-03-24 13:52:55 +0100

    Implement Freatue

---
+++ Feature.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Feature
+{
+    public static double Average(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+            throw new ArgumentException("Tablica liczb nie może być pusta ani null.");
+
+        int sum = 0;
+        foreach (int number in numbers)
+            sum += number;
+
+        return (double)sum / numbers.Length;
+    }
+}
+
