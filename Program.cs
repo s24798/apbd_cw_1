7c0472485c3a29d1f75fca542cb798ab79f39e7e
s24798 <s24798@pjwstk.edu.pl>
2024-03-24 13:54:06 +0100

    Test Feature

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("Modyfikacja 1");
 Console.WriteLine("Modyfikacja 2");
 Console.WriteLine("Modyfikacja 3");
+
+var numbrs = new int[] { 1, 2, 3, 4, 5, 6 };
+Console.WriteLine(Feature.Average(numbrs));
